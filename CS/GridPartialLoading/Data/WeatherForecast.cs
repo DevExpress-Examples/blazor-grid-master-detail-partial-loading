95628aaf82ea90a011a3d99fe7f02ee986bd42d3
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2023-08-31 13:54:22 +0400

    initial commit

---
+++ CS/GridPartialLoading/Data/WeatherForecast.cs
@@ -0,0 +1,9 @@
+namespace GridPartialLoading.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ CS/GridPartialLoading/Data/WeatherForecastService.cs
@@ -0,0 +1,20 @@
+namespace GridPartialLoading.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
