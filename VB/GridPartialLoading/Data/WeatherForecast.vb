c56600e9143b90fdb83b30815e462d4c2e1292eb
DevExpressExampleBot <fakeEmail>
2023-08-31 16:06:00 +0400

    autogenerate VB

---
+++ VB/GridPartialLoading/Data/WeatherForecast.vb
@@ -0,0 +1,9 @@
+Namespace GridPartialLoading.Data
+
+    Public Class WeatherForecast
+
+        Public Property [Date] As DateTime
+
+        Public Property Summary As String?
+    End Class
+End Namespace
